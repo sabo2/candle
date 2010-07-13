3da209f8b7e5bd86f9a674c3c86d2155db63db04
saboten <none@none>
2010-07-13 15:37:22 +0900

    ・Canvas利用時のsetOffsetLinePath関数で、beginPath()を実行していなかった為に   以前に指定したパスが毎回描画されてしまうようになる不具合を修正

--- source/Camp.js
+++ source/Camp.js
@@ -1,4 +1,4 @@
-// Camp.js rev86
+// Camp.js rev87
  
 (function(){
 
@@ -950,6 +950,7 @@ CanvasRenderingContext2D_wrapper.prototype = {
 
 	setLinePath : function(){
 		var _args = arguments, _len = _args.length;
+		this.context.beginPath();
 		for(var i=0,len=_len-((_len|1)?1:2);i<len;i+=2){
 			var a1 = (this.isedge ? _mr(_args[i]  +this.OFFSETX) : _args[i]  +this.OFFSETX);
 				a2 = (this.isedge ? _mr(_args[i+1]+this.OFFSETY) : _args[i+1]+this.OFFSETY);
@@ -960,6 +961,7 @@ CanvasRenderingContext2D_wrapper.prototype = {
 	},
 	setOffsetLinePath : function(){
 		var _args = arguments, _len = _args.length, m=[_args[0]+this.OFFSETX,_args[1]+this.OFFSETY];
+		this.context.beginPath();
 		for(var i=2,len=_len-((_len|1)?1:2);i<len;i+=2){
 			m[i]   = _args[i]   + m[0];
 			m[i+1] = _args[i+1] + m[1];
