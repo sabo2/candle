a90be519ead676c9e5c1ee473a5cf582348ac154
sabo2 <none@none>
2011-05-19 03:09:04 +0900

    外部から呼び出される可能性があるので、srcのURLを変更した

--- test/candle.debug.js
+++ test/candle.debug.js
@@ -1,10 +1,10 @@
 (function(){
 	var _doc = document;
-	_doc.write('<script type="text/javascript" src="../source/candle.core.js"></script>');
-	_doc.write('<script type="text/javascript" src="../source/candle.vector.js"></script>');
-	_doc.write('<script type="text/javascript" src="../source/candle.svg.js"></script>');
-	_doc.write('<script type="text/javascript" src="../source/candle.canvas.js"></script>');
-	_doc.write('<script type="text/javascript" src="../source/candle.sl.js"></script>');
-	_doc.write('<script type="text/javascript" src="../source/candle.vml.js"></script>');
-	_doc.write('<script type="text/javascript" src="../source/candle.core2.js"></script>');
+	_doc.write('<script type="text/javascript" src="../../candle/source/candle.core.js"></script>');
+	_doc.write('<script type="text/javascript" src="../../candle/source/candle.vector.js"></script>');
+	_doc.write('<script type="text/javascript" src="../../candle/source/candle.svg.js"></script>');
+	_doc.write('<script type="text/javascript" src="../../candle/source/candle.canvas.js"></script>');
+	_doc.write('<script type="text/javascript" src="../../candle/source/candle.sl.js"></script>');
+	_doc.write('<script type="text/javascript" src="../../candle/source/candle.vml.js"></script>');
+	_doc.write('<script type="text/javascript" src="../../candle/source/candle.core2.js"></script>');
 })();
