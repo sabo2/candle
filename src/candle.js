33368a7803cf7df6af99d8db68b8e79bd26be8be
sabo2 <none@none>
2014-09-15 18:40:03 +0900

    Gruntfile: pzprv3プロジェクトを参考に、ファイルの生成方法を変更しました
    --HG--
    branch : stable

--- src/candle.js
+++ src/candle.js
@@ -1,19 +1,39 @@
 (function(){
-	var dir="", srcs=document.getElementsByTagName('script');
-	for(var i=0;i<srcs.length;i++){
-		var result = srcs[i].src.match(/^(.*\/)candle\.js$/);
-		if(result){
-			if(result[1].match(/\/$/)){ dir = result[1];}
-			else{ dir = result[1]+'/';}
-			break;
+	var component = [
+		"candle.core",
+		"candle.base",
+		"candle.svg",
+		"candle.canvas",
+		"candle.vml"
+	];
+
+	var isbrowser = true;
+	try{ isbrowser = !exports;}
+	catch(e){}
+
+	if(isbrowser){
+		var dir = (function getpath(){
+			var srcs=document.getElementsByTagName('script');
+			for(var i=0;i<srcs.length;i++){
+				var result = srcs[i].src.match(/^(.*\/)candle\.js$/);
+				if(result){
+					if(result[1].match(/\/$/)){ return result[1];}
+					else{ return result[1]+'/';}
+				}
+			}
+			return "";
+		})();
+		
+		for(var i=0; i<component.length; i++){
+			var file = dir+component[i]+".js";
+			document.write('<script type="text/javascript" src="'+file+'"></script>');
 		}
 	}
+	else{
+		component.unshift("common/intro");
+		component.push   ("common/outro");
 
-	var _doc = document;
-	_doc.write('<script type="text/javascript" src="'+dir+'candle.core.js"></script>');
-	_doc.write('<script type="text/javascript" src="'+dir+'candle.base.js"></script>');
-
-	_doc.write('<script type="text/javascript" src="'+dir+'candle.svg.js"></script>');
-	_doc.write('<script type="text/javascript" src="'+dir+'candle.canvas.js"></script>');
-	_doc.write('<script type="text/javascript" src="'+dir+'candle.vml.js"></script>');
+		var dir = "src/";
+		exports.files = component.map(function(mod){ return dir+mod+".js";});
+	}
 })();
