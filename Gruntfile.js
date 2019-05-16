fccf653b465adfa3f5b52846a7b73f7820ab04df
sabo2 <sabo2@users.noreply.github.com>
2019-05-16 23:45:54 +0900

    Remove grunt package dependency

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt){
-  var pkg = grunt.file.readJSON('package.json'), deps = pkg.devDependencies;
-  for(var plugin in deps){ if(plugin.match(/^grunt\-/)){ grunt.loadNpmTasks(plugin);}}
-  
-  grunt.initConfig({
-    clean: ['dist/*', 'pzpr-canvas-*.tgz', '*.svg']
-  });
-};
-
